62c9e8aea840f861d6d6949ea780e841f0719d86
kazem <kazemzavari.69@gmail.com>
2023-12-02 03:34:54 +0330

    update5

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,28 +1,29 @@
 
-import {menuItems} from "../constants/menuItems"
+import { menuItems } from "../constants/menuItems"
 import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // import { useTheme } from "@emotion/react";
 
 const NavMenu = () => {
-//   const theme = useTheme();
+  //   const theme = useTheme();
   return (
     <>
-     
-        <div component="nav" className="main-menu  ">
+
+      <div component="nav" className="main-menu  ">
         {/* border-bottom: 3px solid transparent; */}
-          <ul className="flex ">
-            {menuItems.map((item, index) => (
-              <li key={index} className="mr-1 px-[8px] xl:px-[10px] ">
-                <Link  className="navbar__link "
-                
+        <ul className="flex ">
+          {menuItems.map((item, index) => (
+            <li key={index} className="mr-1 px-[8px] xl:px-[10px] ">
+              <NavLink className={({ isActive }) => isActive ? "navbar__link--active" : "navbar__link"}
+
                 to={item.link}>
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
- 
+                {item.title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </div>
+
     </>
   );
 };
