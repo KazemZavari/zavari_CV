5d1fd18952ce342c75ab5a161edb67f279628fb2
kazem <kazemzavari.69@gmail.com>
2024-04-21 00:09:58 +0330

    update skills

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -3,7 +3,7 @@ import 'animate.css';
 const PageTitle = ({ children }) => {
     return (
         <h4 className='flex justify-center my-4 lg:mt-8  font-OswaldBold text-[30px] 
-        animate__heartBeat animate__backInDown'>
+        animate__heartBeat animate_ animate__backInDown'>
             {children}
         </h4>
     )
